8b178eddf4a7a283704bf01b813e2372c3d2b04c
Clinton Ecker <clint@arstechnica.com>
2009-04-05 16:22:39 -0500

    adding a setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='google_analytics',
+      version='0.1',
+      description='A simple Django application to integrate Google Analytics into your projects',
+      author='Clint Ecker',
+      author_email='me@clintecker.com',
+      url='http://github.com/clintecker/django-google-analytics/tree/master',
+      packages=['google_analytics','google_analytics.templatetags',],
+      package_data={'google_analytics': ['templates/google_analytics/*.html']},
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+      )
