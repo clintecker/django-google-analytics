78e823f27730be54a10817fd09b7353b1308e4c4
kevin fricovsky <kevin@howiworkdaily.com>
2009-08-02 10:35:15 -0400

    made the project 1.* compliant and fixed a bug with a hardcoded analytics ID, now displays anayltics ID value set in admin properly

--- google_analytics/models.py
+++ google_analytics/models.py
@@ -2,10 +2,9 @@ from django.db import models
 from django.conf import settings
 from django.contrib.sites.models import Site
 
-if getattr(settings, 'GOOGLE_ANALYTICS_MODEL', False):
-    class Analytics(models.Model):
-        site = models.ForeignKey(Site, edit_inline=models.TABULAR, max_num_in_admin=1, min_num_in_admin=1)
-        analytics_code = models.CharField(blank=True, max_length=100, core=True)
+class Analytics(models.Model):
+    site = models.ForeignKey(Site, edit_inline=models.TABULAR, max_num_in_admin=1, min_num_in_admin=1)
+    analytics_code = models.CharField(blank=True, max_length=100, core=True)
 
-        def __unicode__(self):
-            return u"%s" % (self.analytics_code)
+    def __unicode__(self):
+        return u"%s" % (self.analytics_code)
