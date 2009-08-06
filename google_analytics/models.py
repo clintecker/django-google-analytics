553a5b219f378777105c200931b2e7bd19c5f42f
kevin fricovsky <kevin@howiworkdaily.com>
2009-08-05 21:38:57 -0400

    removed legacy admin reference in model

--- google_analytics/models.py
+++ google_analytics/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.contrib.sites.models import Site
 
 class Analytics(models.Model):
-    site = models.ForeignKey(Site, edit_inline=models.TABULAR, max_num_in_admin=1, min_num_in_admin=1)
+    site = models.ForeignKey(Site)
     analytics_code = models.CharField(blank=True, max_length=100, core=True)
 
     def __unicode__(self):
